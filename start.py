3e0fe36406ac90dc3a5446db84fa9d4f773889af
freshman <568245349@qq.com>
2016-02-28 22:25:20 +0800

    hanshu

--- start.py
+++
@@ -1,2 +0,0 @@
-movies = ['the holy grail', 'the life of brian', 'the meaningof life']
-print(movies[1])
--- xbing.py
+++ xbing.py
@@ -1,35 +1,14 @@
-fav_movies = ['the holy frail', 'the lifw of brian']
-print(fav_movies[0])
-print(fav_movies[1])
-for each_flick in fav_movies:
-	print(each_flick)
-
-
-count = 0
-while count < len(fav_movies):
-	print(fav_movies[count])
-	count += 1
-
-
-movies = ['the holy grail',1975,'terry jones terry filliam',91,['graham chapman',['jie', 'qiang', 'fei']]]
-print(movies[4][1][2])
-for item in movies:
-	if isinstance(item, list):
-		for name in item:
-			if isinstance(name, list):
-				for num in name:
-					print(num)
-			else:
-				print(name)
-	else:
-		print(item)
-
-
-def isList(the_list):
-	for items in the_list:
-		if isinstance(items,list):
-			isList(items)
+movies = ['the',1957, 'terry', 91, ['graham',['michael', 'john', 'gilliam', 'eric', 'jones']]]
+#""" 这是一个'nester.py'模块，提供一个名为print_lol()的函数，函数的作用是打印列表，其中有可能包含嵌套列表"""
+def print_lol(the_list, level = 0):
+	#"""'the_list', 是这个函数的位置参数，可以是任何python列表或嵌套列表，所指定的列表中的每个数据项
+	#会输出到屏幕上，各数据项各占一行。"""
+	#"""'level = 0', 为函数提供一个缺省值"""
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item, level + 1)
 		else:
-			print(items)
-
-print(isList(movies))
+			for tab_stop in range(level):
+				print("\t", end = '')
+			print(each_item)
+print_lol(movies)
